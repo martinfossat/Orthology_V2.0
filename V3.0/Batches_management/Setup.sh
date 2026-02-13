8cb1c9c23ec2aa154fe58f696a066c34dd5e8a37
martinfossat <martin.fossat@gmail.com>
2026-02-13 14:06:59 +0100

    Desktop first commit

--- V3.0/Batches_management/Setup.sh
+++ V3.0/Batches_management/Setup.sh
@@ -1,5 +1,6 @@
 source ~/.bashrc
 batch_size=25
+start_batch=0
 file_name=Gene_names.txt
 SCRIPT_LOC='../'
 
@@ -14,7 +15,7 @@ mkdir Batches/Standard_output
 awk -v size="${batch_size}" '{print $1 > ("./Batches/Input_names/batch_" int((NR-1)/size) ".txt")}' ${file_name}
 N_batches=$(( ($(wc -l < "$file_name") + batch_size - 1) / batch_size ))
 
-for((i=0;i<${N_batches};i++));
+for((i=${start_batch};i<${N_batches};i++));
 do
 seq_file=./Batches/Sequences/N_${i}.json
 ortho_file=./Batches/Ortho/N_${i}.json
@@ -28,10 +29,10 @@ echo "#SBATCH -e Batches/Standard_output/std"$i".err" >> run.batch
 echo "#SBATCH --ntasks=1" >> run.batch
 echo "#SBATCH --cpus-per-task=1"  >> run.batch
 
-echo python3 ${SCRIPT_LOC}+/1_Get_orthologs_V1.3.py -f ./Batches/Input_names/batch_${i}.txt -of ${ortho_file} -sf ${seq_file} -nf ${names_file} -os drerio -as hsapiens mmusculus>> run.batch
-echo python3 ${SCRIPT_LOC}+/2_Get_IDRs_V2.0.py -of ${ortho_file} -sf ${seq_file} -pf ${seq_prop_file} >> run.batch
-echo python3 ${SCRIPT_LOC}+/3_Add_sequence_properties_V1.1.py -pf ${seq_prop_file} -sf ${seq_file} -of ${ortho_file}  >> run.batch
-echo python3 ${SCRIPT_LOC}+/4_Compare_sequences_V1.3.py -of ${ortho_file} -hf ${homo_file} -sf ${seq_file} -pf ${seq_prop_file} -rs hsapiens -as drerio mmusculus >> run.batch
+echo python3 ${SCRIPT_LOC}/1_Get_orthologs_V1.3.py -f ./Batches/Input_names/batch_${i}.txt -of ${ortho_file} -sf ${seq_file} -nf ${names_file} -os drerio -as hsapiens mmusculus>> run.batch
+echo python3 ${SCRIPT_LOC}/2_Get_IDRs_V2.0.py -of ${ortho_file} -sf ${seq_file} -pf ${seq_prop_file} >> run.batch
+echo python3 ${SCRIPT_LOC}/3_Add_sequence_properties_V1.1.py -pf ${seq_prop_file} -sf ${seq_file} -of ${ortho_file}  >> run.batch
+echo python3 ${SCRIPT_LOC}/4_Compare_sequences_V1.3.py -of ${ortho_file} -hf ${homo_file} -sf ${seq_file} -pf ${seq_prop_file} -rs hsapiens -as drerio mmusculus >> run.batch
 #echo python3 ${SCRIPT_LOC}+/5_Plot_comparison_V1.0.py -nf ${names_file} -hf ${homo_file} -rs hsapiens -as drerio mmusculus >> run.batch
 #echo python3 ${SCRIPT_LOC}+/6_Plot_properties_V2.4.py -f ./Batches/Homology_scores/N_${i}_All_homology.json -rs hsapiens  -as drerio mmusculus >> run.batch
 
